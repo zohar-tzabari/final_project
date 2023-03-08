3177dd3649869e60630c2313e2321a6530a6d5fe
roe <roe1996@gmail.com>
2023-03-08 21:02:55 +0200

    home page

--- client/App.js
+++ client/App.js
@@ -1,13 +1,60 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { Camera, CameraType } from 'expo-camera';
 import React, { useState, useEffect } from 'react';
-import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Button, StyleSheet, Text, TouchableOpacity, Image, View } from 'react-native';
 import axios from 'axios';
+import { useNavigation } from '@react-navigation/native';
 
+function HomePage() {
+  const navigation = useNavigation();
+  const handleButtonPress = (buttonText) => {
+    console.log(`Pressed ${buttonText} button`);
+    navigation.navigate('CameraPage');
+  }
 
-export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome to My App</Text>
+      <View style={styles.buttonColumn}>
+        <View style={styles.buttonRow}>
+          <TouchableOpacity style={[styles.button, styles.buttonTopLeft]} onPress={() => handleButtonPress('Button 1')}>
+            <Image source={require('./assets/key.jpg')} style={styles.buttonImage} />
+            <Text style={styles.buttonText}>Button 1</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.button, styles.buttonTopRight]} onPress={() => handleButtonPress('Button 2')}>
+          <Image source={require('./assets/key.jpg')} style={styles.buttonImage} />
+            <Text style={styles.buttonText}>Button 2</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.buttonRow}>
+          <TouchableOpacity style={[styles.button, styles.buttonMiddleLeft]} onPress={() => handleButtonPress('Button 3')}>
+          <Image source={require('./assets/key.jpg')} style={styles.buttonImage} />
+            <Text style={styles.buttonText}>Button 3</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.button, styles.buttonMiddleRight]} onPress={() => handleButtonPress('Button 4')}>
+          <Image source={require('./assets/key.jpg')} style={styles.buttonImage} />
+            <Text style={styles.buttonText}>Button 4</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.buttonRow}>
+          <TouchableOpacity style={[styles.button, styles.buttonBottomLeft]} onPress={() => handleButtonPress('Button 5')}>
+          <Image source={require('./assets/key.jpg')} style={styles.buttonImage} />
+            <Text style={styles.buttonText}>Button 5</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.button, styles.buttonBottomRight]} onPress={() => handleButtonPress('Button 6')}>
+          <Image source={require('./assets/key.jpg')} style={styles.buttonImage} />
+            <Text style={styles.buttonText}>Button 6</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+function CameraPage() {
   const [hasPermission, setHasPermission] = useState(null);
   const [cameraType, setCameraType] = useState(Camera.Constants.Type.back);
-
 
   useEffect(() => {
     (async () => {
@@ -15,7 +62,6 @@ export default function App() {
       setHasPermission(status === 'granted');
     })();
   }, []);
-
 
   const fetchApi = async() =>
   {
@@ -27,11 +73,9 @@ export default function App() {
     }
   }
 
-
   useEffect(() => {
     fetchApi()
   }, []);
-
 
   if (hasPermission === null) {
     return <View />;
@@ -41,15 +85,12 @@ export default function App() {
     return <Text>No access to camera</Text>;
   }
 
-
-
   function toggleCameraType() {
     setCameraType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
   }
 
-
   return (
-    <View style={styles.container}>
+    <View style={styles.cameraContainer}>
       <Camera style={styles.camera} type={cameraType}>
         <View style={styles.buttonContainer}>
           <TouchableOpacity style={styles.button} onPress={toggleCameraType}>
@@ -76,11 +117,21 @@ async function startStreaming() {
   }
 }
 
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomePage} />
+        <Stack.Screen name="CameraPage" component={CameraPage} options={{ title: 'CameraPage' }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
   camera: {
     flex: 1,
   },
@@ -101,4 +152,65 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: 'bold',
   },
-});
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: '#fff',
+    },
+    title: {
+      fontSize: 24,
+      fontWeight: 'bold',
+      marginBottom: 24,
+    },
+    buttonColumn: {
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      flex: 1,
+    },
+    buttonRow: {
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center',
+      flex: 1,
+    },
+    button: {
+      backgroundColor: '#3f51b5',
+      borderRadius: 8,
+      padding: 5,
+      margin: 0,
+      flex: 1,
+    },
+    buttonText: {
+      color: '#fff',
+      fontSize: 18,
+      fontWeight: 'bold',
+      textAlign: 'center',
+    },
+    buttonImage: {
+      height: 100,
+      width: 100,
+      marginBottom: 10,
+    },
+    buttonTopLeft: {
+      borderTopLeftRadius: 0,
+    },
+    buttonTopRight: {
+      borderTopRightRadius: 0,
+    },
+    buttonMiddleLeft: {
+      borderBottomLeftRadius: 0,
+      borderTopLeftRadius: 0,
+    },
+    buttonMiddleRight: {
+      borderBottomRightRadius: 0,
+      borderTopRightRadius: 0,
+    },
+    buttonBottomLeft: {
+      borderBottomLeftRadius: 0,
+    },
+    buttonBottomRight: {
+      borderBottomRightRadius: 0,
+    },
+  });
