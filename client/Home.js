90eebfb1131f086abb6286bf942de06cb3572f4e
roe <roe1996@gmail.com>
2023-05-09 18:25:45 +0300

    fix bugs

--- client/Home.js
+++ client/Home.js
@@ -1,108 +1,130 @@
 import * as React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, Image, TouchableWithoutFeedback, Platform, NativeModules,Button } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, Image, TouchableWithoutFeedback, Platform, NativeModules, Button } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-//import Sound from 'react-native-sound';
 import { Audio } from 'expo-av';
 
 const { UIManager } = NativeModules;
 UIManager.setLayoutAnimationEnabledExperimental?.(true);
 
 const Home = () => {
+  
   const navigation = useNavigation();
-
+  
+  async function playSound(soundName) {
+    switch (soundName){
+      case "key":
+        so = await Audio.Sound.createAsync( require('./assets/sounds/key.mp3'));
+        break;
+      case "fork":
+        so = await Audio.Sound.createAsync( require('./assets/sounds/fork.mp3'));
+        break;
+      case "spoon":
+        so = await Audio.Sound.createAsync( require('./assets/sounds/spoon.mp3'));
+        break;
+      case "knife":
+        so = await Audio.Sound.createAsync( require('./assets/sounds/knife.mp3'));
+        break;
+        case "bottle":
+        so = await Audio.Sound.createAsync( require('./assets/sounds/bottle.mp3'));
+        break;
+      case "toothbrush":
+        so = await Audio.Sound.createAsync( require('./assets/sounds/toothbrush.mp3'));
+        break;
+    }
+    
+      const { sound } = so;
+      await sound.playAsync();
+  }
+    
   const handleButtonPress = (buttonName) => {
     navigation.navigate('Commera', { buttonName });
   };
 
-  async function playSound() {
-    console.log('Loading Sound');
-    const { sound } = await Audio.Sound.createAsync( require('./assets/sounds/key.mp3'));
-    
-  
-    console.log('Playing Sound');
-    await sound.playAsync();
-  }
-
-  const handleButtonHover = () => {
-    
-
-    playSound();
+  const handleButtonHover = (soundName) => {
+    playSound(soundName);
   };
 
   return (
     <View style={styles.container}>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => handleButtonPress('key')}
-        onPressIn={() => handleButtonHover()}
-        accessible={true}
-        accessibilityRole="button"
-        accessibilityHint="Key"
-        accessibilityLabel="Key"
-      >
-        <Image
-          style={styles.buttonImage}
-          source={require('./assets/key.jpg')}
-        />
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => handleButtonPress('Button 2')}
-        onPressIn={() => handleButtonHover()}
-      >
-        <Image
-          style={styles.buttonImage}
-          source={require('./assets/phone.jpg')}
-        />
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => handleButtonPress('Button 3')}
-        onPressIn={() => handleButtonHover()}
-        accessible={true}
-        accessibilityRole="button"
-        accessibilityHint="Key"
-        accessibilityLabel="Key"
-      >
-        <Image
-          style={styles.buttonImage}
-          source={require('./assets/laptop.jpg')}
-        />
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => handleButtonPress('Button 4')}
-        onPressIn={() => handleButtonHover()}
-      >
-        <Image
-          style={styles.buttonImage}
-          source={require('./assets/umbrella.jpg')}
-        />
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => handleButtonPress('Button 5')}
-        onPressIn={() => handleButtonHover()}
-        accessible={true}
-        accessibilityRole="button"
-        accessibilityHint="Key"
-        accessibilityLabel="Key"
-      >
-        <Image
-          style={styles.buttonImage}
-          source={require('./assets/remote.jpg')}
-        />
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => handleButtonPress('Button 6')}
-        onPressIn={() => handleButtonHover()}
-      >
-        <Image
-          style={styles.buttonImage}
-          source={require('./assets/bag.jpg')}
-        />
-      </TouchableOpacity>
+
+      <View style={styles.row}>
+
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => handleButtonPress('key')}
+          onPressIn={() => handleButtonHover('key')}
+        >
+          <Image
+            style={styles.buttonImage}
+            source={require('./assets/key.jpg')}
+          />
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => handleButtonPress('fork')}
+          onPressIn={() => handleButtonHover('fork')}
+          >
+          <Image
+            style={styles.buttonImageFork}
+            source={require('./assets/fork.jpg')}
+          />
+        </TouchableOpacity>
+
+      </View>
+
+      <View style={styles.row}>
+
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => handleButtonPress('spoon')}
+          onPressIn={() => handleButtonHover('spoon')}
+          >
+          <Image
+            style={styles.buttonImageSpoon}
+            source={require('./assets/spoon.jpg')}
+          />
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => handleButtonPress('knife')}
+          onPressIn={() => handleButtonHover('knife')}
+          >
+          <Image
+            style={styles.buttonImageSpoon}
+            source={require('./assets/knife.jpg')}
+          />
+        </TouchableOpacity>
+
+      </View>
+
+      <View style={styles.row}>
+
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => handleButtonPress('bottle')}
+          onPressIn={() => handleButtonHover('bottle')}
+          >
+          <Image
+            style={styles.buttonImage}
+            source={require('./assets/bottle.jpg')}
+          />
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => handleButtonPress('toothbrush')}
+          onPressIn={() => handleButtonHover('toothbrush')}
+          >
+          <Image
+            style={styles.buttonImageToot}
+            source={require('./assets/toothbrush.jpg')}
+          />
+        </TouchableOpacity>
+
+      </View>
+
     </View>
   );
 };
@@ -113,22 +135,43 @@ const styles = StyleSheet.create({
     backgroundColor: '#ffffffff',
     alignItems: 'center',
     justifyContent: 'center',
+    flexDirection: 'column'
+  },
+  row: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+    width: '100%'
   },
   button: {
     flexDirection: 'row',
     backgroundColor: '#ffffffff',
-    padding: 20,
-    borderRadius: 0,
-    //marginVertical: 10,
+    //padding: 20,
+    //borderRadius: 0,
     alignItems: 'center',
     justifyContent: 'center',
-    width: '100%',
+    width: '50%',
+    height: '100%',
     alignItems: 'center',
+    //borderWidth: 1,
+    //borderColor: 'black'
   },
   buttonImage: {
-    width: 67,
-    height: 75,
-    //marginRight: 10,
+    width: '80%',
+    height: '80%',
+  },
+  buttonImageSpoon: {
+    width: '100%',
+    height: '60%',
+  },
+  buttonImageFork: {
+    width: '100%',
+    height: '80%',
+  },
+  buttonImageToot: {
+    width: '100%',
+    height: '20%',
   },
   buttonText: {
     color: '#ffffff',
@@ -137,4 +180,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default Home;
+export default Home;  
