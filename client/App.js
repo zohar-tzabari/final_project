7ce51e85ba0d5cbc841acc54b96546ebb560445e
zohar-tzabari <80604271+zohar-tzabari@users.noreply.github.com>
2023-05-11 20:58:24 +0300

    [front][Home] add home components

---
+++ client/App.js
@@ -0,0 +1,18 @@
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+import HomeContainer from './HomeContainer';
+import SearchObject from './SearchObject';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="HomePresenter" component={HomeContainer} />
+        <Stack.Screen name="SearchObject" component={SearchObject} />
+              </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ client/HomeContainer.js
@@ -0,0 +1,53 @@
+import React, { useEffect } from "react";
+import HomePresenter from "./HomePresenter";
+import { useNavigation } from "@react-navigation/native";
+import { Audio } from "expo-av";
+
+
+const HomeContainer = () => {
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    // Clean up any resources when the component unmounts
+    return () => {
+      Audio.Sound.unloadAsync();
+    };
+  }, []);
+
+
+  const playSound = async (soundName) => {
+    try {
+      const soundMap = {
+        key: require('./assets/sounds/key.mp3'),
+        fork: require('./assets/sounds/fork.mp3'),
+        spoon: require('./assets/sounds/spoon.mp3'),
+        knife: require('./assets/sounds/knife.mp3'),
+        bottle: require('./assets/sounds/bottle.mp3'),
+        toothbrush: require('./assets/sounds/toothbrush.mp3'),
+      };
+
+      const sound = new Audio.Sound();
+      await sound.loadAsync(soundMap[soundName]);
+      await sound.playAsync();
+    } catch (error) {
+      console.log("Error playing sound:", error);
+    }
+  };
+
+  const handleButtonPress = (buttonName) => {
+    navigation.navigate("SearchObject", { buttonName });
+  };
+
+  const handleButtonHover = (soundName) => {
+    playSound(soundName);
+  };
+
+  return (
+    <HomePresenter
+      handleButtonPress={handleButtonPress}
+      handleButtonHover={handleButtonHover}
+    />
+  );
+};
+
+export default HomeContainer;
